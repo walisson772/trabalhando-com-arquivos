43623f871a5dc74d83c76bff944cb4f9ee77a8c3
Walisson Santos <walissonsantost008@gmail.com>
2025-04-17 15:46:22 -0300

    Primeiro teste usando java para ler arquivos

---
+++ untitled/src/aplication/Program.java
@@ -0,0 +1,27 @@
+package aplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args){
+
+        File file = new File("D:\\exercicio\\trabalhando-com-arquivos\\untitled\\src\\in");
+        Scanner sc = null;
+        try{
+            sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: "+ e.getMessage());
+        }
+        finally {
+            if (sc != null){
+                sc.close();
+            }
+        }
+    }
+}
